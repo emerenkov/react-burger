0e6e07d42df9f54cd734846319e84e40a19c0e0b
Evgeniy Marchal <evgeniymarchal@yandex.ru>
2022-06-13 15:10:44 +0300

    complited version1

--- src/components/OrderDetails/OrderDetails.jsx
+++ src/components/OrderDetails/OrderDetails.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import orderDetailsStyle from './OrderDetails.module.css';
-import PropTypes from 'prop-types';
+import PropTypese from 'prop-types';
 import types from "../../utils/types";
 
 const OrderDetails = ({orderNumber}) => {
-    console.log(orderNumber);
+
     return(
         <div className={`${orderDetailsStyle.container} pt-30 pb-30`}>
             <h2 className={`${orderDetailsStyle.titleNumbers} text text_type_digits-large`}>{orderNumber.order.number}</h2>
@@ -16,8 +16,8 @@ const OrderDetails = ({orderNumber}) => {
     )
 }
 
-OrderDetails.propTypes = {
-    orderNumber: PropTypes.number.isRequired
-}
+OrderDetails.propTypese = {
+    orderNumber: PropTypese.number.isRequired,
+};
 
 export default OrderDetails;
